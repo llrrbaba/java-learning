68c19896f2212af2ee6fc6ae9cc76fb1c265e9d7
chengzhichao <chengzhichao@58.com>
2019-11-29 17:15:30 +0800

    log4j 配置文件

--- log/log4j/src/main/java/cn/rocker/log4j/LogDemo.java
+++ log/log4j/src/main/java/cn/rocker/log4j/LogDemo.java
@@ -1,5 +1,8 @@
 package cn.rocker.log4j;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 /**
  * Log4j 日志框架学习
  *
@@ -7,8 +10,16 @@ package cn.rocker.log4j;
  * @create: 2019-11-29 14:23
  * @since:
  **/
+@Slf4j
 public class LogDemo {
 
-
+    @Test
+    public void testLogLevel(){
+        log.trace("this is trace");
+        log.debug("this is debug");
+        log.info("this is info");
+        log.warn("this is warn");
+        log.error("this is error");
+    }
 
 }
