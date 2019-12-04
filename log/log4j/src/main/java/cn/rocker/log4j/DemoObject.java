de2ecef9af6561604269d2c7332f1a9d8b44f3d7
chengzhichao <chengzhichao@58.com>
2019-12-04 20:42:04 +0800

    测试自定义logger

---
+++ log/log4j/src/main/java/cn/rocker/log4j/DemoObject.java
@@ -0,0 +1,20 @@
+package cn.rocker.log4j;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * @author: rocker
+ * @create: 2019-12-04 15:07
+ * @since:
+ **/
+@Data
+public class DemoObject {
+
+    private String name;
+    private int age;
+    private String home;
+    private String add;
+    private Map map;
+}
