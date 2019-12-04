78f04194dbf772f913bc5728bb36d2ed0e2e7ec9
chengzhichao <chengzhichao@58.com>
2019-12-04 20:41:53 +0800

    测试自定义logger

--- log/log4j/src/main/java/cn/rocker/log4j/LogDemo.java
+++ log/log4j/src/main/java/cn/rocker/log4j/LogDemo.java
@@ -1,7 +1,13 @@
 package cn.rocker.log4j;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Log4j 日志框架学习
@@ -20,6 +26,24 @@ public class LogDemo {
         log.info("this is info");
         log.warn("this is warn");
         log.error("this is error");
+    }
+
+    @Test
+    public void testFengling() {
+        Logger fengling = LoggerFactory.getLogger("fengling");
+        Map<String, Object> logmap = new HashMap();
+        logmap.put("aaa","111");
+        logmap.put("bbb","222");
+        logmap.put("ccc","333");
+
+        DemoObject demoObject = new DemoObject();
+        demoObject.setAdd("aaa");
+        demoObject.setAge(333);
+        demoObject.setHome("ddd");
+        demoObject.setName("rrrr");
+        demoObject.setMap(logmap);
+
+        fengling.info(JSON.toJSONString(logmap));
     }
 
 }
