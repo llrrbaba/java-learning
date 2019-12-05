68d1917e9e6a0489f593e82dd077ecb648c96cfd
rocker <rocker_pg@163.com>
2019-12-05 08:50:20 +0800

    ...

---
+++ juc/src/main/java/cn/rocker/juc/future/completablefuture3/AllofDemo.java
@@ -0,0 +1,32 @@
+package cn.rocker.juc.future.completablefuture3;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author rocker
+ * @date 2019/12/04 23:41
+ * @since V1.0
+ */
+public class AllofDemo {
+
+    @Test
+    public void test() {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "tony");
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "cafei");
+
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> "aaron");
+
+        CompletableFuture.allOf(future1, future2, future3)
+                .thenApply(v ->
+                        Stream.of(future1, future2, future3)
+                                .map(CompletableFuture::join)
+                                .collect(Collectors.joining(" ")))
+                .thenAccept(System.out::print);
+    }
+
+}
