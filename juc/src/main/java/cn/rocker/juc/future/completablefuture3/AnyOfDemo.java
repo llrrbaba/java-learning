d3240d516afb16d7fe32f027e4651df52aec7456
chengzhichao <chengzhichao@58.com>
2019-12-05 16:01:23 +0800

    anyof allof

--- juc/src/main/java/cn/rocker/juc/future/completablefuture3/AnyOfDemo.java
+++ juc/src/main/java/cn/rocker/juc/future/completablefuture3/AnyOfDemo.java
@@ -3,8 +3,13 @@ package cn.rocker.juc.future.completablefuture3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author: rocker
@@ -17,29 +22,32 @@ public class AnyOfDemo {
     @Test
     public void test() {
         Random rand = new Random();
-        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(rand.nextInt(1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             log.info("from future1");
+            return "from future1";
         });
-        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(rand.nextInt(1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             log.info("from future2");
+            return "from future2";
         });
-        CompletableFuture<Void> future3 = CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(rand.nextInt(1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             log.info("from future3");
+            return "from future3";
         });
 
         CompletableFuture<Object> future = CompletableFuture.anyOf(future1, future2, future3);
@@ -49,12 +57,54 @@ public class AnyOfDemo {
             // System.out.println(future.get());
 
             // 非阻塞式地回调
-            future.whenComplete((v,t) -> log.info("any done"));
+            future.whenComplete((v, t) -> log.info("any done:{}", v));
             System.in.read();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+
+    @Test
+    public void test2() {
+        StringBuilder result = new StringBuilder();
+        List<String> messages = Arrays.asList("a", "b", "c");
+        List<CompletableFuture> futures = messages.stream()
+                .map(msg -> CompletableFuture.completedFuture(msg).thenApply(s -> s.toUpperCase()))
+                .collect(Collectors.toList());
+        CompletableFuture.anyOf(futures.toArray(new CompletableFuture[futures.size()])).whenComplete((res, th) -> {
+            if (th == null) {
+                assertTrue(isUpperCase((String) res));
+                result.append(res);
+            }
+        });
+        assertTrue("Result was empty", result.length() > 0);
+    }
+
+    public boolean isUpperCase(String str) {
+
+        for (int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if (Character.isLowSurrogate(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void test3(){
+        StringBuilder result = new StringBuilder();
+        List<String> messages = Arrays.asList("a", "b", "c");
+        List<CompletableFuture> futures = messages.stream()
+                .map(msg -> CompletableFuture.completedFuture(msg).thenApply(s -> s.toUpperCase()))
+                .collect(Collectors.toList());
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).whenComplete((v, th) -> {
+            futures.forEach(cf -> assertTrue(isUpperCase((String) cf.getNow(null))));
+            result.append("done");
+        });
+        assertTrue("Result was empty", result.length() > 0);
     }
 
 }
