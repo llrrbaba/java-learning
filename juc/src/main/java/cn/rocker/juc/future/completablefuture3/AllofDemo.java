ee47ec1de231435483d6d29a13ee0083867f4b95
chengzhichao <chengzhichao@58.com>
2019-12-05 15:58:49 +0800

    anyof allof

--- juc/src/main/java/cn/rocker/juc/future/completablefuture3/AllofDemo.java
+++ juc/src/main/java/cn/rocker/juc/future/completablefuture3/AllofDemo.java
@@ -18,39 +18,39 @@ public class AllofDemo {
 
     @Test
     public void test() throws Exception {
-        long start = System.currentTimeMillis();
-        log.info("start:{}", start);
-
-        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             log.info("future1:{}", 111);
+            return "111";
         });
 
-        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             log.info("future2:{}", 222);
+            return "222";
         });
 
-        CompletableFuture<Void> future3 = CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             log.info("future3:{}", 333);
+            return "333";
         });
 
         // 非阻塞式地回调
         CompletableFuture.allOf(future1, future2, future3).whenComplete((v,t) -> {
-            log.info("all done");
+            log.info("all done:{}", v);
         });
 
         System.in.read();
@@ -64,8 +64,7 @@ public class AllofDemo {
 ////                              .collect(Collectors.joining(" ")))
 //                .thenAccept(str -> log.info("result:{}", str));
 //        voidCompletableFuture.get();
-        long end = System.currentTimeMillis();
-        log.info("duration:{}", end - start);
     }
+
 
 }
